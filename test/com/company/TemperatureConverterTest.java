00ad1a567751ccc6d0fd0df1d12534a320b5b090
valente alvarez <valvarez@basecampcodingacademy.org>
2018-01-24 10:19:06 -0600

    Change the class so it would change read the string then it would change the string number and turns it to a double. Then it would take the information through the class that will convert the temperature for the user. then change the the test so it would get 100% test coverage.

--- test/com/company/TemperatureConverterTest.java
+++ test/com/company/TemperatureConverterTest.java
@@ -8,47 +8,47 @@ class TemperatureConverterTest {
 
     @Test
     void KelvintoFahrenheit() {
-        TemperatureConverter converter = new TemperatureConverter("k", "f");
+        TemperatureConverter converter = new TemperatureConverter('k', "f");
         assertEquals(32, converter.conversion(273.15));
     }
     @Test
     void KelvintoCelsius() {
-        TemperatureConverter converter = new TemperatureConverter("k", "c");
+        TemperatureConverter converter = new TemperatureConverter('k', "c");
         assertEquals(0, converter.conversion(273.15));
     }
     @Test
     void KelvintoKelvin() {
-        TemperatureConverter converter = new TemperatureConverter("k", "k");
+        TemperatureConverter converter = new TemperatureConverter('k', "k");
         assertEquals(273.15, converter.conversion(273.15));
     }
     @Test
     void FahrenheittoFahrenheit() {
-        TemperatureConverter converter = new TemperatureConverter("f", "f");
+        TemperatureConverter converter = new TemperatureConverter('f', "f");
         assertEquals(12, converter.conversion(12));
     }
     @Test
     void FahrenheittoCelsius() {
-        TemperatureConverter converter = new TemperatureConverter("f", "c");
+        TemperatureConverter converter = new TemperatureConverter('f', "c");
         assertEquals(0, converter.conversion(32));
     }
     @Test
     void FahrenheittoKelvin() {
-        TemperatureConverter converter = new TemperatureConverter("f", "k");
+        TemperatureConverter converter = new TemperatureConverter('f', "k");
         assertEquals(273.15, converter.conversion(32));
     }
     @Test
     void CelsiustoCelsius() {
-        TemperatureConverter converter = new TemperatureConverter("c", "c");
+        TemperatureConverter converter = new TemperatureConverter('c', "c");
         assertEquals(32, converter.conversion(32));
     }
     @Test
     void CelsiustoFahrenheit() {
-        TemperatureConverter converter = new TemperatureConverter("c", "f");
+        TemperatureConverter converter = new TemperatureConverter('c', "f");
         assertEquals(32, converter.conversion(0));
     }
     @Test
     void CelsiustoKelvin() {
-        TemperatureConverter converter = new TemperatureConverter("c", "k");
+        TemperatureConverter converter = new TemperatureConverter('c', "k");
         assertEquals(273.15, converter.conversion(0));
     }
 }
