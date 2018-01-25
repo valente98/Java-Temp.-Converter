0aa81edfb0dfeb4c88966bba12636b49f072a5cc
valente alvarez <valvarez@basecampcodingacademy.org>
2018-01-25 10:00:08 -0600

    change the character to string then went back and change all of the character to string."

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,32 @@
 package com.company;
+import sun.security.util.Length;
+
 import java.util.Scanner;
 
 
 public class Main {
-//    public static void p(Object a) { System.out.println(a);}
 
     public static void main(String[] args) {
         System.out.println("Welcome to Temperature Converter.");
         Scanner using = new Scanner(System.in);
         String x = using.next();
         double temperature = Double.valueOf(x.substring(0, x.length() - 1));
-        Character source = x.charAt(x.length() - 1);
+        Character unit_temp = Character.toLowerCase(x.charAt(x.length() - 1));
+        String source = Character.toString(unit_temp);
         using.next();
         String converter = using.next();
-        TemperatureConverter tempconverter = new TemperatureConverter(source, converter);
-        if (converter.equals("f")) {
-            System.out.println(tempconverter.conversion(temperature) + " degrees Fahrenheit");
-        } else if (converter.equals("c")){
-            System.out.println(tempconverter.conversion(temperature) + " degrees Celsius");
-        } else if (converter.equals("k")){
-            System.out.println(tempconverter.conversion(temperature) + " degrees Kelvin");
-        }
+            TemperatureConverter tempconverter = new TemperatureConverter(source, converter);
+            if (converter.length() > 1) {
+                System.out.println("Sorry is Invalid Choice.");
+            }else if  (source.length() > 1) {
+                System.out.println("Sorry is Invalid Choice.");
+            } else if (converter.toLowerCase().equals("f")) {
+                System.out.println(tempconverter.conversion(temperature) + " degrees Fahrenheit");
+            } else if (converter.toLowerCase().equals("c")) {
+                System.out.println(tempconverter.conversion(temperature) + " degrees Celsius");
+            } else if (converter.toLowerCase().equals("k")) {
+                System.out.println(tempconverter.conversion(temperature) + " degrees Kelvin");
+            }
+
     }
 }
